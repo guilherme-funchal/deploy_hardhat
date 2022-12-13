54ed4764cabf3a88a9938c510736c11be7556630
Guilherme Funchal da Silva <guilherme.funchal@gmail.com>
2022-12-13 08:11:54 -0300

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+// task action function receives the Hardhat Runtime Environment as second argument
+task(
+  "blockNumber",
+  "Prints the current block number",
+  async (_, { ethers }) => {
+    await ethers.provider.getBlockNumber().then((blockNumber) => {
+      console.log("Current block number: " + blockNumber);
+    });
+  }
+);
+
+module.exports = {};
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+};
